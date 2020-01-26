a1e4010c9fd48870af1136fd0bd11fae3224a487
Taehyun Kim <persona7548@gmail.com>
2020-01-26 15:59:26 +0900

    https://www.acmicpc.net/problem/2161
    https://www.acmicpc.net/problem/2161
    
    cpp/ queue

---
+++ 2161.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	queue<int> q;
+	int n, i;
+	cin >> n;
+	for (i = 1; i <= n; i++)
+		q.push(i);
+	while (q.size()!=1) {
+		cout << q.front() << " ";
+		q.pop();
+		if (q.size() != 1){
+			i = q.front();
+			q.pop();
+			q.push(i);
+		}
+	}
+	cout << q.front();
+	return 0;
+}

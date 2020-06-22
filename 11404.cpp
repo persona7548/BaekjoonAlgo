334058978ef5b6134bdde14c10f87aa20884a156
Taehyun Kim <persona7548@gmail.com>
2020-06-22 16:00:37 +0900

    Floyd Warshall Algorithm
    https://www.acmicpc.net/problem/11404

--- 11404.cpp
+++ 11404.cpp
@@ -1,17 +1,19 @@
 #include <algorithm>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
 	int graph[101][101] = { 0, };
-	fill(graph[0], graph[0] + 101 * 101, 100001);
+	fill(graph[0], graph[0] + 101 * 101, 9900001);
 	int n, m, u, v, w;
 	cin >> n >> m;
 	for (int i = 0; i < m; i++) {
 		cin >> u >> v >> w;
-		if(graph[u][v] > w)
+		if (graph[u][v] > w)
 			graph[u][v] = w;
 	}
+
 	for (int i = 1; i <= n; i++)
 		graph[i][i] = 0;
 
@@ -23,12 +25,13 @@ int main() {
 
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++)
-			if (graph[i][j] == 100001)
+			if (graph[i][j] == 9900001)
 				cout << 0 << " ";
 			else
 				cout << graph[i][j] << " ";
 		cout << "\n";
 	}
+
 
 	return 0;
 }
